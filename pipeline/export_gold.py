c9754b011f8979c48ed524baeb6a3e8c5ed29af6
amelia boukri <boukriamelia215@gmail.com>
2026-05-23 14:57:38 +0200

    Ajout exports silver et gold en local

---
+++ pipeline/export_gold.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import logging
+import os
+from pyspark.sql import SparkSession
+
+# LOGS
+if not os.path.exists("/opt/logs"):
+    os.makedirs("/opt/logs")
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("/opt/logs/export_gold.txt"),
+        logging.StreamHandler()
+    ]
+)
+log = logging.getLogger("export_gold")
+
+# SPARK SESSION
+spark = (
+    SparkSession.builder
+    .appName("export_gold")
+    .config("spark.jars", "/opt/jars/mysql-connector-java-8.0.28.jar")
+    .getOrCreate()
+)
+log.info("SparkSession demarree")
+
+mysql_url = "jdbc:mysql://mysql:3306/datamarts"
+mysql_props = {
+    "user": "admin",
+    "password": "admin2024",
+    "driver": "com.mysql.cj.jdbc.Driver"
+}
+
+datamarts = ["dm_marketing", "dm_risque", "dm_bi"]
+
+for table in datamarts:
+    log.info("Export {} vers HDFS Gold...".format(table))
+    df = spark.read.jdbc(url=mysql_url, table=table, properties=mysql_props)
+    (
+        df.write
+        .mode("overwrite")
+        .parquet("hdfs://namenode:9000/data/gold/{}".format(table))
+    )
+    log.info("{} exporte dans Gold : {} lignes".format(table, df.count()))
+
+log.info("Export Gold termine avec succes")
+spark.stop()
